8c55378b201bba7a6b3bdb102988eaa815c9a1f5
apoorv singh <tofu.develops@gmail.com>
2024-10-14 20:46:12 +0530

    feat: God level shit

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,15 @@
 import {Pearl} from "../src"
 
+import { Fields, Provider } from "../src/types";
+
 
 
 const client = new Pearl({ apiKey: "quark_ls_KUXR4U$$2$R3LgF^OkXx9k+" })
 .context([])
-.defineWorkflow("Open google.com and go to google finance")
-.once()
+.defineWorkflow("Go to google.com, search google finance find me stocks of 10 different companies and you can use url hopping, all 10 companies have to be different, memorize their stock prices and charts on the page and give me a detailed comparision of their prices as successmessage")
+.instances(10);
+
+
 
 
 client.on("update", (msg: any) => {
